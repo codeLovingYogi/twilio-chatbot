6de47bfc9eda96781445718e9e028900e34ca488
Felice Ho <yogilovingcode@gmail.com>
2017-06-02 23:10:00 -0400

    Try adding sms reply

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'My flask app!'
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+
+    resp = MessagingResponse().message("How can we help you?")
+    return str(resp)
 
 if __name__ == "__main__":
     app.run()
